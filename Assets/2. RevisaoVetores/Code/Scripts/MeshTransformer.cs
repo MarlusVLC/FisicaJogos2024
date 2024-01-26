dba6110e2f39058cad0afddf0ca0fa77470951e4
Marlus Vinicius <marlus.cabral@compasso.com.br>
2024-01-26 11:54:26 -0300

    Método de translação de vetores

---
+++ Assets/2. RevisaoVetores/Code/Scripts/MeshTransformer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace _2._RevisaoVetores
+{
+    public class MeshTransformer : MonoBehaviour
+    {
+        [SerializeField] private MeshFilter meshFilter;
+        [Space] [SerializeField] private float tx, ty;
+
+        private Mesh mesh => meshFilter.mesh;
+        
+        private void Translate2D(float tx, float ty)
+        {
+            var vertices = mesh.vertices;
+            for (var i = 0; i < mesh.vertexCount; i++)
+            {
+                vertices[i] = new Vector3(vertices[i].x + tx, vertices[i].y + ty);
+            }
+
+            mesh.vertices = vertices;
+        }
+
+        private void Update()
+        {
+            Translate2D(tx * Time.deltaTime, ty * Time.deltaTime);
+        }
+    }
+}
