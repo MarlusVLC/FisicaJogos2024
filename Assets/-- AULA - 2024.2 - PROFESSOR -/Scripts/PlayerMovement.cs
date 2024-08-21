8ade84ce5158beac42d4f70ebe0f0c6a3e81d836
Marlus Vinicius <marlus.cabral@compasso.com.br>
2024-08-21 19:20:30 -0300

    (consume) Andrei scripts com coyoteTime + variableJump
    Signed-off-by: Marlus Vinicius <marlus.cabral@compasso.com.br>

--- Assets/-- AULA - 2024.2 - PROFESSOR -/Scripts/PlayerMovement.cs
+++ Assets/-- AULA - 2024.2 - PROFESSOR -/Scripts/PlayerMovement.cs
@@ -8,13 +8,20 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5;
     public float jumpVelocity = 10;
     public float jumpBufferTime = 1f;
+    public float coyoteTime = 1f;
+    public float variableJumpMinTime = 1f;
 
     private float hMoveInput = 0f;
     private float jumpBuffer = 0f;
+    private float coyoteTimer = 0f;
+    private float variableJumpTimer = 0f;
+    private bool cancelJumpInput = false;
+
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
+
     void Update()
     {
         //Inputs horizontais do jogador
@@ -23,27 +30,64 @@ public class PlayerMovement : MonoBehaviour
         {
             jumpBuffer = jumpBufferTime;
         }
+        if(Input.GetButtonUp("Jump"))
+        {
+            cancelJumpInput = true;
+        }
     }
+
     //Função que determina se o player pode pular, usando input buffer
     private bool ShouldJump()
     {
         return IsGrounded() && jumpBuffer > 0f;
     }
+
     private void Jump()
     {
         rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
         jumpBuffer = 0f;
+        coyoteTimer = 0f;
+        variableJumpTimer = variableJumpMinTime;
     }
+
     private bool IsGrounded()
     {
         int mask = ~LayerMask.GetMask("Player");//Projeção da esfera contra todas layers, exceto a do jogador (~)
         RaycastHit2D hit = Physics2D.CircleCast(transform.position, 0.5f, Vector2.down, 0.4f, mask);
-        return hit.collider != null;
+        if (hit.collider != null && rb != null && rb.velocity.y <= 0f)
+        {
+            coyoteTimer = coyoteTime;
+        }
+        return coyoteTimer > 0f || hit.collider != null;
     }
+
     private void Move()
     {
         rb.velocity = new Vector2(hMoveInput * moveSpeed, rb.velocity.y);
     }
+
+    private void TryCancelJump()
+    {
+        //Checagem do timer, pois o pulo só pode ser cancelado após o tempo mínimo
+        if (variableJumpTimer < 0f)
+        {
+            if (cancelJumpInput)
+            {
+                //Cancelamento da velocidade vertical. Se a velocidade for menor que 0, não fazemos nada
+                rb.velocity = new Vector2(rb.velocity.x, Mathf.Min(rb.velocity.y, 0f));
+            }
+            cancelJumpInput = false;
+        }
+    }
+
+    //Atualização dos timers relacionados a técnicas de movimentação
+    private void UpdateTimers()
+    {
+        jumpBuffer -= Time.fixedDeltaTime;
+        coyoteTimer -= Time.fixedDeltaTime;
+        variableJumpTimer -= Time.fixedDeltaTime;
+    }
+
     private void FixedUpdate()
     {
         Move();
@@ -51,8 +95,10 @@ public class PlayerMovement : MonoBehaviour
         {
             Jump();
         }
-        jumpBuffer -= Time.fixedDeltaTime;
+        TryCancelJump();
+        UpdateTimers();
     }
+
     private void OnDrawGizmos()
     {
         //Desenho de uma esfera colorida para representar a variável isGrounded e a área de checagem.
